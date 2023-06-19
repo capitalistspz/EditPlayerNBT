d4e7773533bc03facd1df58bf18264aa5d939987
capitalistspz <keipitalists@proton.me>
2023-06-19 01:57:19 +0100

    the missing shit

--- src/main/java/com/capitalistspz/editplayernbt/mixin/EditPlayerNBT.java
+++ src/main/java/com/capitalistspz/editplayernbt/mixin/EditPlayerNBT.java
@@ -1,4 +1,29 @@
-package com.capitalistspz.editplayernbt;
+package com.capitalistspz.editplayernbt.mixin;
 
+import net.minecraft.command.EntityDataObject;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(EntityDataObject.class)
 public class EditPlayerNBT {
+    @Shadow @Final private Entity entity;
+
+    @Inject(method="setNbt", at=@At("HEAD"), cancellable = true)
+    void allowPlayerNbtSet(NbtCompound nbt, CallbackInfo ci){
+        if (this.entity instanceof PlayerEntity){
+            UUID uUID = this.entity.getUuid();
+            this.entity.readNbt(nbt);
+            this.entity.setUuid(uUID);
+            ci.cancel();
+        }
+    }
 }
